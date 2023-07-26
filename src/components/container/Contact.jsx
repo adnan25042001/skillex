faa4aa3c179acb47a9eec6991b76bf085dccdc10
Adnan <adnan.hussain.136660@gmail.com>
2023-07-26 16:50:15 +0530

    completed contact section

--- src/components/container/Contact.jsx
+++ src/components/container/Contact.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Contact = () => {
-    return <div className="section" id="contact">Contact</div>;
+    return (
+        <div className="section" id="contact">
+            <div className="text-center max-w-[600px] mx-auto">
+                <div className="sm:text-3xl text-2xl font-bold mb-5">
+                    Subscribe Newsletter
+                </div>
+                <p className="text-sm leading-7 text-gray">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Repudiandae asperiores veritatis nulla consequatur sunt,
+                    modi illum nostrum dolores voluptatem laborum molestiae
+                    omnis esse ratione reiciendis sint. Officiis soluta culpa,
+                    cupiditate cumque ad, vitae odit placeat nulla natus
+                    reprehenderit dolorem nihil.
+                </p>
+                <motion.form
+                    initial={{ scale: 0 }}
+                    whileInView={{ scale: 1 }}
+                    transition={{ duration: 0.3 }}
+                    className="mt-5"
+                >
+                    <input
+                        type="text"
+                        placeholder="Enter your email address"
+                        className="sm:p-3 p-2 outline-none text-sm shadow-md sm:w-72 w-60"
+                    />
+                    <button className="text-sm text-white bg-Teal sm:p-3 p-2 shadow-md font-bold">
+                        Subscribe
+                    </button>
+                </motion.form>
+            </div>
+        </div>
+    );
 };
 
 export default Contact;
